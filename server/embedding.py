b896612f72e01049443acc93369a92835b254216
zuanki <nguyenxuanhoakhtn@gmail.com>
2024-01-08 20:49:59 +0700

    Implemented dynamic link server

--- server/embedding.py
+++ server/embedding.py
@@ -1,11 +1,17 @@
 # !pip install sentence-transformers
 # !pip install flask flask_cors pyngrok
+# !pip install supabase
 from PIL import Image
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from pyngrok import ngrok
 import requests
+from supabase import create_client, Client
 from sentence_transformers import SentenceTransformer, util
+
+url = "https://rxyltmzhoyhvkptdmoog.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4eWx0bXpob3lodmtwdGRtb29nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ3MTMwNDksImV4cCI6MjAyMDI4OTA0OX0.5NhtnvurC2iKIWS1rFjpYBwdxK-09sbyCEI5y02NR0k"
+supabase = create_client(url, key)
 
 ngrok.set_auth_token("2aIeZIzuDvUnBBGDiqJmfuhYIlw_3uY9Qhqud8C6yd7tAyheN")
 
@@ -54,6 +60,10 @@ def add():
 
 
 url = ngrok.connect(5000).public_url
+
+supabase.table("Server").update(
+    {"server_url": url}).eq("server_name", "Kaggle_Embedding").execute()
+
 print('Global URL:', url)
 if __name__ == '__main__':
     app.run(debug=False)
--- server/milvus.js
+++ server/milvus.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require("cors");
 const bodyParser = require('body-parser');
 const { MilvusClient, DataType } = require('@zilliz/milvus2-sdk-node');
+const createClient = require('@supabase/supabase-js')
 require('dotenv').config();
 
 // Connect to milvus
@@ -10,6 +11,12 @@ const client = new MilvusClient({
     address: process.env.MILVUS_URI,
     token: process.env.MILVUS_TOKEN,
 });
+
+// Connect to supabase
+const supabase = createClient.createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
+)
 
 
 const app = express();
@@ -23,25 +30,32 @@ app.post('/api/search', (req, res) => {
         type: req.body.type,
     };
 
-    axios.post(`https://example.com/api/embedding`, user_query)
-        .then(response => {
-            const vector = response.data.embedding;
-            client.search({
-                collection_name: "JapanGuideImageEmbedding",
-                data: vector,
-                output_fields: ["name", "subtitle", "description", "link_image"],
-                limit: 5
-            }).then(searchResponse => {
-                // console.log(searchResponse);
-                res.json(searchResponse.results); // Send the searchResponse as JSON
-            }).catch(searchErr => {
-                // console.log(searchErr);
-                res.status(500).send("Error in searching"); // Sending a 500 error status in case of search failure
+
+    supabase.from('Server').select('server_url').eq('server_name', "Kaggle_Embedding").then(r => {
+        const url = r.data[0].server_url;
+        axios.post(`${url}/api/embedding`, user_query)
+            .then(response => {
+                const vector = response.data.embedding;
+                client.search({
+                    collection_name: "JapanGuideImageEmbedding",
+                    data: vector,
+                    output_fields: ["name", "subtitle", "description", "link_image"],
+                    limit: 5
+                }).then(searchResponse => {
+                    // console.log(searchResponse);
+                    res.json(searchResponse.results); // Send the searchResponse as JSON
+                }).catch(searchErr => {
+                    // console.log(searchErr);
+                    res.status(500).send("Error in searching"); // Sending a 500 error status in case of search failure
+                });
+            }).catch(axiosErr => {
+                // console.log(axiosErr.response.data);
+                res.status(500).send("Axios error"); // Sending a 500 error status in case of Axios failure
             });
-        }).catch(axiosErr => {
-            // console.log(axiosErr.response.data);
-            res.status(500).send("Axios error"); // Sending a 500 error status in case of Axios failure
-        });
+    }
+    ).catch(err => {
+        console.log(err)
+    })
 });
 
 
