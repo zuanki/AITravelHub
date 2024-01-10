a378fcb40bd72cbaf169a9ca0e460aa8ea43dc90
zuanki <nguyenxuanhoakhtn@gmail.com>
2024-01-10 19:42:00 +0700

    Update UI components

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useState } from "react"
 import axios from "axios"
+import Link from "next/link"
 
 type SearchResult = {
     score: number,
@@ -10,6 +11,25 @@ type SearchResult = {
     description: string,
     link_image: string,
 }
+
+const data: SearchResult[] = [
+    {
+        "score": 0.5,
+        "id": "1",
+        "name": "Tokyo",
+        "subtitle": "Tokyo is the capital of Japan.",
+        "description": "Tokyo is the capital of Japan. It is the center of the Greater Tokyo Area, and the most populous metropolitan area in the world. It is the seat of the Japanese government and the Imperial Palace, and the home of the Japanese Imperial Family.",
+        "link_image": "https://media.cntraveler.com/photos/60341fbad7bd3b27823c9db2/4:3/w_4624,h_3468,c_limit/Tokyo-2021-GettyImages-1208124099.jpg"
+    },
+    {
+        "score": 0.9,
+        "id": "2",
+        "name": "Hokkaido",
+        "subtitle": "Hokkaido is the second largest island of Japan.",
+        "description": "Hokkaido is the second largest island of Japan, and the largest and northernmost prefecture. The Tsugaru Strait separates Hokkaido from Honshu. The two islands are connected by the undersea railway Seikan Tunnel.",
+        "link_image": "https://www.jal.co.jp/vn/vn/guide-to-japan/destinations/articles/hokkaido/best-parks-and-nature-attractions/_jcr_content/root/responsivegrid/sectioncontainer/image_1041888335.coreimg.jpeg/1636979413394.jpeg"
+    }
+]
 
 export default function Page() {
     const [userQuery, setUserQuery] = useState<string>("")
@@ -28,11 +48,14 @@ export default function Page() {
                 query: userQuery,
                 type: "text"
             }
-            const response = axios.post("http://localhost:5000/api/search", dataForm).then((response) => {
-                setSearchResults(response.data)
-            }).catch((error) => {
-                console.log(error)
-            })
+
+            // const response = axios.post("http://localhost:5000/api/search", dataForm).then((response) => {
+            //     setSearchResults(response.data)
+            // }).catch((error) => {
+            //     console.log(error)
+            // })
+
+            setSearchResults(data)
         }
     }
 
@@ -65,9 +88,11 @@ export default function Page() {
                             {
                                 searchResults.map((result) => {
                                     return (
-                                        <li key={result.name}>
-                                            <h3>{result.name}</h3>
-                                            <img src={result.link_image} alt={result.name} className="rounded-md" />
+                                        <li key={result.name} className="my-4">
+                                            <Link className="font-bold my-2"
+                                                href={`/destination/${result.id}`}
+                                            >{result.name}</Link>
+                                            <img src={result.link_image} alt={result.name} className="rounded-md w-[200px] h-[200px]" />
                                         </li>
                                     )
                                 })
