88d516a97461d824d40a4f8489bb2edb1fcde0ec
zuanki <nguyenxuanhoakhtn@gmail.com>
2024-01-12 18:16:59 +0700

    Improve UI on search component

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,54 +10,9 @@ export default function RootLayout(props: React.PropsWithChildren) {
   return (
     <Providers>
       <html lang='en'>
-        <body className='bg-gray-50'>
-          <section className={styles.container}>
-            {/* <Nav /> */}
-
-            {/* <header className={styles.header}>
-              <img src="/logo.svg" className={styles.logo} alt="logo" />
-            </header> */}
-
-            <main className={styles.main}>{props.children}</main>
-
-            {/* <footer className={styles.footer}>
-              <span>Learn </span>
-              <a
-                className={styles.link}
-                href="https://reactjs.org/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                React
-              </a>
-              <span>, </span>
-              <a
-                className={styles.link}
-                href="https://redux.js.org/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Redux
-              </a>
-              <span>, </span>
-              <a
-                className={styles.link}
-                href="https://redux-toolkit.js.org/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Redux Toolkit
-              </a>
-              ,<span> and </span>
-              <a
-                className={styles.link}
-                href="https://react-redux.js.org/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                React Redux
-              </a>
-            </footer> */}
+        <body className='bg-[#EDF1D6] text-[#40513B] font-poppins'>
+          <section >
+            <main >{props.children}</main>
           </section>
         </body>
       </html>
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -11,6 +11,9 @@ import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 import { storage } from '@/firebase/config';
 import moment from 'moment';
 import { set } from 'firebase/database';
+import { TbGridDots } from "react-icons/tb";
+import { AiOutlineLike } from "react-icons/ai";
+import { AiOutlineDislike } from "react-icons/ai";
 
 type SearchResult = {
   score: number;
@@ -39,6 +42,66 @@ const data: SearchResult[] = [
     subtitle: 'Hokkaido is the second largest island of Japan.',
     description:
       'Hokkaido is the second largest island of Japan, and the largest and northernmost prefecture. The Tsugaru Strait separates Hokkaido from Honshu. The two islands are connected by the undersea railway Seikan Tunnel.',
+    link_image:
+      'https://www.jal.co.jp/vn/vn/guide-to-japan/destinations/articles/hokkaido/best-parks-and-nature-attractions/_jcr_content/root/responsivegrid/sectioncontainer/image_1041888335.coreimg.jpeg/1636979413394.jpeg',
+  },
+  {
+    score: 0.5,
+    id: '1',
+    name: 'Tokyo',
+    subtitle: 'Tokyo is the capital of Japan.',
+    description:
+      'Tokyo is the capital of Japan. It is the center of the Greater Tokyo Area, and the most populous metropolitan area in the world. It is the seat of the Japanese government and the Imperial Palace, and the home of the Japanese Imperial Family.',
+    link_image:
+      'https://media.cntraveler.com/photos/60341fbad7bd3b27823c9db2/4:3/w_4624,h_3468,c_limit/Tokyo-2021-GettyImages-1208124099.jpg',
+  },
+  {
+    score: 0.9,
+    id: '2',
+    name: 'Hokkaido',
+    subtitle: 'Hokkaido is the second largest island of Japan.',
+    description:
+      'Hokkaido is the second largest island of Japan, and the largest and northernmost prefecture. The Tsugaru Strait separates Hokkaido from Honshu. The two islands are connected by the undersea railway Seikan Tunnel.',
+    link_image:
+      'https://www.jal.co.jp/vn/vn/guide-to-japan/destinations/articles/hokkaido/best-parks-and-nature-attractions/_jcr_content/root/responsivegrid/sectioncontainer/image_1041888335.coreimg.jpeg/1636979413394.jpeg',
+  },
+  {
+    score: 0.5,
+    id: '1',
+    name: 'Tokyo',
+    subtitle: 'Tokyo is the capital of Japan.',
+    description:
+      'Tokyo is the capital of Japan. It is the center of the Greater Tokyo Area, and the most populous metropolitan area in the world. It is the seat of the Japanese government and the Imperial Palace, and the home of the Japanese Imperial Family.',
+    link_image:
+      'https://media.cntraveler.com/photos/60341fbad7bd3b27823c9db2/4:3/w_4624,h_3468,c_limit/Tokyo-2021-GettyImages-1208124099.jpg',
+  },
+  {
+    score: 0.9,
+    id: '2',
+    name: 'Hokkaido',
+    subtitle: 'Hokkaido is the second largest island of Japan.',
+    description:
+      'Hokkaido is the second largest island of Japan, and the largest and northernmost prefecture. The Tsugaru Strait separates Hokkaido from Honshu. The two islands are connected by the undersea railway Seikan Tunnel.',
+    link_image:
+      'https://www.jal.co.jp/vn/vn/guide-to-japan/destinations/articles/hokkaido/best-parks-and-nature-attractions/_jcr_content/root/responsivegrid/sectioncontainer/image_1041888335.coreimg.jpeg/1636979413394.jpeg',
+  },
+  {
+    score: 0.5,
+    id: '1',
+    name: 'Tokyo',
+    subtitle: 'Tokyo is the capital of Japan.',
+    description:
+      'Tokyo is the capital of Japan. It is the center of the Greater Tokyo Area, and the most populous metropolitan area in the world. It is the seat of the Japanese government and the Imperial Palace, and the home of the Japanese Imperial Family.',
+    link_image:
+      'https://media.cntraveler.com/photos/60341fbad7bd3b27823c9db2/4:3/w_4624,h_3468,c_limit/Tokyo-2021-GettyImages-1208124099.jpg',
+  },
+  {
+    score: 0.9,
+    id: '2',
+    name: 'Hokkaido',
+    subtitle: 'Hokkaido is the second largest island of Japan.',
+    description:
+      'Tokyo is the capital of Japan. It is the center of the Greater Tokyo Area, and the most populous metropolitan area in the world. It is the seat of the Japanese government and the Imperial Palace, and the home of the Japanese Imperial Family. Hokkaido is the second largest island of Japan, and the largest and northernmost prefecture. The Tsugaru Strait separates Hokkaido from Honshu. The two islands are connected by the undersea railway Seikan Tunnel.',
     link_image:
       'https://www.jal.co.jp/vn/vn/guide-to-japan/destinations/articles/hokkaido/best-parks-and-nature-attractions/_jcr_content/root/responsivegrid/sectioncontainer/image_1041888335.coreimg.jpeg/1636979413394.jpeg',
   },
@@ -107,7 +170,7 @@ export default function Search() {
   };
 
   // Handle keydown
-  const handleKeyDown = async (event: React.KeyboardEvent<HTMLInputElement> | React.MouseEvent<HTMLButtonElement>) => {
+  const handleKeyDown = async (event: React.KeyboardEvent<HTMLInputElement> | React.MouseEvent<HTMLButtonElement> | React.KeyboardEvent<HTMLTextAreaElement>) => {
     if (event.type === 'click' || (event as React.KeyboardEvent).key === 'Enter') {
       if (searchInput === '' && !fileImage) return;
       if (fileInputRef.current) {
@@ -187,89 +250,118 @@ export default function Search() {
     }
   };
 
+  // Auto Resize Textarea
+  const autoResize = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    e.target.style.height = 'auto';
+    e.target.style.height = e.target.scrollHeight + 'px';
+  };
+
+  // Truncate string
+  const truncate = (str: string, n: number) => {
+    return str?.length > n ? str.substring(0, n - 1) + ' ...' : str;
+  };
+
   return (
-    <div className='mt-12 flex w-full items-center justify-center '>
-      <div className='mth-fit w-[800px] rounded-xl bg-[#1f1f1d]'>
-        <div className='space-y-4 px-8 py-2'>
-          <input
-            type='text'
+    <div>
+      <div className='flex justify-center items-center text-4xl font-bold m-12'>
+        Where knowledge begins
+      </div>
+      <div className='flex justify-center items-center'>
+        <div className='w-[800px] p-4 justify-center items-center rounded-md border-2 border-[#9DC08B]'>
+          <textarea
+            className='p-2 bg-[#EDF1D6] w-full resize-none overflow-hidden focus:right-0 focus:outline-none'
             placeholder='Ask anything . . .'
+            onInput={autoResize}
             value={searchInput}
             onChange={(e) => {
               setSearchInput(e.target.value);
               setIsSubmit(false);
               setUrlImage('');
             }}
-            className='h-[50px] rounded-t-xl bg-transparent text-lg font-semibold text-white outline-none'
             onKeyDown={handleKeyDown}
           />
-
-          <div className='flex flex-col'>
-            <div className='w-full space-y-2'>
-              <input ref={fileInputRef} type='file' name='' id='file' hidden onChange={(e) => addFileImage(e)} />
-
-              {fileImage && (
-                <div className='flex space-x-2'>
+          <div className=''>
+            <input ref={fileInputRef} type='file' name='' id='file' hidden onChange={(e) => addFileImage(e)} />
+            {
+              fileImage && (
+                <div className='flex p-2'>
                   <div className='relative h-12'>
                     <Image
                       src={blobUrl}
                       alt='file image'
                       width={1000}
                       height={1000}
-                      className='h-12 w-12 rounded-lg object-cover'
+                      className='h-12 w-12 rounded-lg '
                     />
                     <button onClick={() => popFileImage()}>
-                      <IoIosCloseCircle className='absolute -right-2 -top-2 text-neutral-500' />
+                      <IoIosCloseCircle className='absolute -right-2 -top-2 text-[#9DC08B]' />
                     </button>
                   </div>
                 </div>
-              )}
-            </div>
-
-            <div className='mt-2 flex w-full justify-between'>
-              <label htmlFor='file' className='flex w-fit items-center rounded-full px-3 py-2 hover:bg-neutral-600'>
-                <FiPlusCircle className='cursor-pointer text-2xl text-gray-100' />
-                <p className='ml-2 font-semibold text-gray-300'>Attach</p>
+              )
+            }
+            <div className='flex justify-between items-center'>
+              <label htmlFor='file'
+                className='flex font-medium justify-between items-center hover:bg-[#9DC08B] rounded-full px-2 py-1 cursor-pointer'
+              >
+                <FiPlusCircle className='text-md' />
+                <p className='ml-2'>Attach</p>
               </label>
-
-              <button type='button' onClick={handleKeyDown} >
-                <BsArrowRightCircleFill className='text-2xl text-gray-400 hover:text-gray-100 cursor-pointer' />
+              <button type='button' onClick={handleKeyDown}>
+                <div className='p-2 hover:bg-[#9DC08B] rounded-full cursor-pointer'>
+                  <BsArrowRightCircleFill className='text-xl' />
+                </div>
               </button>
             </div>
           </div>
         </div>
-        {isSubmit && (
-          <div className='text-white mx-10'>
-            <div className='flex justify-between mx-8 items-center'>
-              <div className=' text-xl text-white'>{previousInput}</div>
-
-              <button onClick={() => clearSearchResults()}>
-                <IoIosCloseCircle className='text-gray-400 text-2xl' />
-              </button>
+      </div >
+      <div className='flex justify-center items-center mt-10'>
+        <div className='w-[800px] '>
+          <div className='font-bold text-xl'>
+            <div className='flex items-center'>
+              <TbGridDots />
+              <p className='ml-2'>Search Results</p>
             </div>
-            {urlImage != '' ? (
-              <div className='flex flex-col items-center justify-center'>
-                <img src={urlImage} alt='image' className='max-h-96 w-1/2 rounded-lg p-10' />
-              </div>
-            ) : null}
+          </div>
+          <div>
             {
-              searchResults.length > 0 && (
-                <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 my-10">
-                  {searchResults.map((result, idx) => {
-                    return (
-                      <div key={result.subtitle} className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                        <a href={`/destination/${idx}`}>
-                          <img className="rounded-lg" src={result.link_image} alt={result.name} />
-                        </a>
+              data.map((result, idx) => {
+                return (
+                  <div>
+                    <div className='my-4 flex items-center' key={idx}>
+                      <div>
+                        <img src={result.link_image} alt={result.name} className='rounded-lg  w-[100px] h-[100px]' />
                       </div>
-                    );
-                  })}
-                </div>
-              )
+                      <div className='ml-4 flex flex-col justify-center w-full' >
+                        <div className='flex justify-between items-center w-full mb-2'>
+                          <div className='font-bold text-lg'>
+                            {result.name}
+                          </div>
+                          <div className='text-xs font-bold'>
+                            <div className='flex items-center'>
+                              <AiOutlineLike className='text-xl text-[#9DC08B]' />
+                              <p className='ml-2'>1.2k</p>
+
+                              <AiOutlineDislike className='text-xl text-[#9DC08B] ml-4' />
+                              <p className='ml-2'>1.2k</p>
+                            </div>
+                          </div>
+                        </div>
+                        <div className='text-sm'>
+                          {truncate(result.description, 250)}
+                        </div>
+                      </div>
+
+                    </div>
+                    <hr className='border-[#9DC08B]' />
+                  </div>
+                );
+              })
             }
           </div>
-        )}
+        </div>
       </div>
-    </div>
+    </div >
   );
 }
