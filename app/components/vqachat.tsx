71dca432cc51b6703d8f6ef45557e1f43aa10cb9
PhamMinhVuong <100543300+PhamMinhVuong21020425@users.noreply.github.com>
2024-02-01 00:50:37 +0700

    Update UI vqa chatbot

--- app/components/vqachat.tsx
+++ app/components/vqachat.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import { CiCirclePlus } from "react-icons/ci";
 import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 import { storage } from '@/firebase/config';
@@ -10,12 +10,14 @@ import axios from 'axios';
 type ChatProps = {
     message: string;
     isBot: boolean;
+    image: string;
 };
 
 const data: ChatProps[] = [
     {
         message: 'Hi, I am VQA Chatbot. How can I help you?',
         isBot: true,
+        image: ''
     }
 ];
 
@@ -35,8 +37,6 @@ const getCurrentDatetime = (): string => {
 export default function Chat() {
     let resMsg = '';
 
-    let url = 'https://media.cntraveler.com/photos/63482b255e7943ad4006df0b/16:9/w_2560%2Cc_limit/tokyoGettyImages-1031467664.jpeg';
-
     const [chatHistory, setChatHistory] = useState<ChatProps[]>(data);
 
     const [input, setInput] = useState<string>('');
@@ -47,11 +47,9 @@ export default function Chat() {
 
     const textareaRef = useRef<HTMLTextAreaElement>(null);
 
+    const chatContainerRef = useRef<HTMLDivElement>(null);
+
     const [blobUrl, setBlobUrl] = useState<string>('');
-
-    const [urlImage, setUrlImage] = useState<string>('');
-
-    const [isSubmit, setIsSubmit] = useState<boolean>(false);
 
     const [isLoaded, setIsLoaded] = useState<boolean>(true);
 
@@ -59,12 +57,15 @@ export default function Chat() {
 
     const [fileImageChat, setFileImageChat] = useState<File | null>(null);
 
+    useEffect(() => {
+        scrollToBottom();
+    }, [chatHistory]);
+
     const addFileImage = (e: React.ChangeEvent<HTMLInputElement>) => {
         let file = e.target.files?.[0] as File;
         if (file) {
             setBlobUrl(URL.createObjectURL(file));
             setFileImageChat(file);
-            setIsSubmit(false);
         }
     };
 
@@ -79,7 +80,6 @@ export default function Chat() {
         }
 
         setFileImageChat(null);
-        setUrlImage('');
     };
 
     const clearInputs = () => {
@@ -87,10 +87,10 @@ export default function Chat() {
         setFileImageChat(null);
     };
 
-    const sendMessage = (message: string) => {
+    const sendMessage = (message: string, url: string) => {
         setChatHistory((prev) => [
             ...prev,
-            { message: message, isBot: false },
+            { message: message, isBot: false, image: url },
         ]);
     }
 
@@ -100,28 +100,33 @@ export default function Chat() {
             {
                 message: message,
                 isBot: true,
+                image: ''
             },
         ]);
     }
 
     // Handle send message
-    const handleSendMessage = () => {
+    const handleSendMessage = (url: string) => {
         if (input) {
-            sendMessage(input);
-            setInput('');
+            sendMessage(input, url);
         }
     };
 
     // Handle input change
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setInput(e.target.value);
-        setIsSubmit(false);
     }
 
     // Auto Resize Textarea
     const autoResize = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         e.target.style.height = 'auto';
         e.target.style.height = e.target.scrollHeight + 'px';
+    };
+
+    const scrollToBottom = () => {
+        if (chatContainerRef.current) {
+            chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+        }
     };
 
     // Handle keydown
@@ -131,7 +136,7 @@ export default function Chat() {
             | React.MouseEvent<HTMLButtonElement>
             | React.KeyboardEvent<HTMLTextAreaElement>
     ) => {
-        if (event.type === 'click') {
+        if (event.type === 'click' || (event as React.KeyboardEvent).key === 'Enter') {
             setIsLoaded(false);
             // setSearchResults(data);
             if (textareaRef.current) {
@@ -179,15 +184,10 @@ export default function Chat() {
                         }
                     );
                 });
-                url = await promiseUpload;
-            } else {
-                setIsSubmit(true);
             }
 
-            // url = await promiseUpload;
-            setUrlImage(url);
-            setIsSubmit(true);
-            handleSendMessage();
+            const url = await promiseUpload;
+            handleSendMessage(url);
             clearInputs();
 
             // POST
@@ -218,25 +218,42 @@ export default function Chat() {
     return (
         <div className="">
             <div className="font-bold text-lg mb-2">VQA</div>
-            <div className="overflow-auto h-96 no-scrollbar">
+            <div ref={chatContainerRef} className="overflow-y-auto h-96 no-scrollbar">
                 {chatHistory.map((chat, index) => (
-                    <div
-                        key={index}
-                        className={`flex ${chat.isBot ? 'justify-start' : 'justify-end'
-                            } mb-2`}
-                    >
+                    <div>
                         <div
-                            className={`rounded-md p-2  ${chat.isBot
-                                ? 'bg-[#F6F6F6] text-black' // Adjust for bot messages
-                                : 'bg-[#9DC08B] text-white'
-                                }`}
+                            key={index}
+                            className={`flex ${chat.isBot ? 'justify-start' : 'justify-end'} ${chat.image !== '' ? '' : 'mb-3'}`}
                         >
-                            {chat.isBot ? (
-                                <div className="text-xs">{chat.message}</div>
-                            ) : (
-                                <div className="text-xs">{chat.message}</div>
-                            )}
+                            <div
+                                className={`rounded-md p-2 max-w-80 overflow-x-hidden  ${chat.isBot
+                                    ? 'bg-[#F6F6F6] text-black' // Adjust for bot messages
+                                    : 'bg-[#9DC08B] text-white'
+                                    }`}
+                            >
+                                {chat.isBot ? (
+                                    <div className="text-xs break-words">{chat.message}</div>
+                                ) : (
+                                    <div className="text-xs break-words">{chat.message}</div>
+                                )}
+                            </div>
                         </div>
+                        {chat.image !== '' ? (
+                            <div className={`flex ${chat.isBot ? 'justify-start' : 'justify-end'} mb-3`}>
+                                <div className="mt-2">
+                                    <img
+                                        src={chat.image}
+                                        alt="Image"
+                                        style={{
+                                            height: "150px",
+                                            width: "auto",
+                                            display: "block",
+                                        }}
+                                        className="transition-opacity duration-500 rounded-md bg-[#F6F6F6]"
+                                    />
+                                </div>
+                            </div>
+                        ) : null}
                     </div>
                 ))}
             </div>
@@ -275,6 +292,7 @@ export default function Chat() {
                         placeholder="Enter your message here"
                         value={input}
                         onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(e)}
+                        onKeyDown={handleKeyDown}
                     />
                 </div>
                 <button
